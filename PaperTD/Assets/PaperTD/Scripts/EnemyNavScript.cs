047704db88c74dfd2cf0ecb8561d0f6d2ebcc029
Michael Makarenko <zugidor@tutanota.com>
2024-04-25 20:57:30 +0100

    Scale enemy movement

--- PaperTD/Assets/PaperTD/Scripts/EnemyNavScript.cs
+++ PaperTD/Assets/PaperTD/Scripts/EnemyNavScript.cs
@@ -1,39 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // Class with Hard Coded 3 Node Path
 public class EnemyNavScript : MonoBehaviour
 {
-    public float movementSpeed = 5;
+	public float movementSpeed = 5;
+	float scale;
+	string currentTargetTag = "MiddleNode";
+	Vector3 targetPos;
 
-    string currentTargetTag = "MiddleNode";
-    Vector3 target;
+	// Start is called before the first frame update
+	void Start()
+	{
+		targetPos = GameObject.FindGameObjectWithTag(currentTargetTag).transform.position;
+		scale = gameObject.transform.localScale.x;
+		movementSpeed *= scale;
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = GameObject.FindGameObjectWithTag(currentTargetTag).transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        float distanceFromNode = (gameObject.transform.position - target).magnitude;
-        if (distanceFromNode > 0.1)
-        {
-            Vector3 travelDirection = (target - gameObject.transform.position).normalized;
-            gameObject.transform.position += movementSpeed * Time.deltaTime * travelDirection;
-            Vector3 forwardOnPlane = gameObject.transform.forward;
-            forwardOnPlane.y = 0;
-            float angle = Vector3.Angle(forwardOnPlane.normalized, travelDirection);
-            gameObject.transform.Rotate(new Vector3(0, 1, 0), angle);
-        }
-        else if (currentTargetTag == "MiddleNode")
-        {
-            currentTargetTag = "EndNode";
-            target = GameObject.FindGameObjectWithTag(currentTargetTag).transform.position;
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		float distanceFromNode = (gameObject.transform.position - targetPos).magnitude;
+		if (distanceFromNode > 0.1 * scale)
+		{
+			Vector3 travelDirection = (targetPos - gameObject.transform.position).normalized;
+			gameObject.transform.position += movementSpeed * Time.deltaTime * travelDirection;
+			Vector3 forwardOnPlane = gameObject.transform.forward;
+			forwardOnPlane.y = 0;
+			float angle = Vector3.Angle(forwardOnPlane.normalized, travelDirection);
+			gameObject.transform.Rotate(new Vector3(0, 1, 0), angle);
+		}
+		else if (currentTargetTag == "MiddleNode")
+		{
+			currentTargetTag = "EndNode";
+			targetPos = GameObject.FindGameObjectWithTag(currentTargetTag).transform.position;
+		}
+	}
 }
