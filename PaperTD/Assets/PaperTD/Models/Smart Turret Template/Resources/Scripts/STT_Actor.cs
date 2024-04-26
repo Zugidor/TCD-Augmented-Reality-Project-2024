1b806226488b0ede743424512d7a78cdf6532395
Michael Makarenko <zugidor@tutanota.com>
2024-04-26 22:20:39 +0100

    Clean up imported turret scripts code

--- PaperTD/Assets/PaperTD/Models/Smart Turret Template/Resources/Scripts/STT_Actor.cs
+++ PaperTD/Assets/PaperTD/Models/Smart Turret Template/Resources/Scripts/STT_Actor.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
-public class ActorParameters {
-	
+public class ActorParameters
+{
 	[Tooltip("Actor's Health")]
 	public float toughness;
 	[Tooltip("Threshold of the applied force")]
 	public float armor;
 	public float damageFactor;
 }
-	
+
 [System.Serializable]
-public class ActorFX {
-	
+public class ActorFX
+{
 	[Tooltip("Spawn this GameObject when the turrent is hitting")]
 	public GameObject damageFX;
 	[Tooltip("Spawn this GameObject when the turrent is destroyed")]
@@ -22,15 +20,15 @@ public class ActorFX {
 }
 
 [System.Serializable]
-public class ActorAudio {
-
+public class ActorAudio
+{
 	public AudioClip destroyClip;
 }
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(AudioSource))]
-public class STT_Actor : MonoBehaviour {
-
+public class STT_Actor : MonoBehaviour
+{
 	public ActorParameters parameters;
 	public ActorFX VFX;
 	public ActorAudio SFX;
@@ -39,44 +37,45 @@ public class STT_Actor : MonoBehaviour {
 
 	void Awake()
 	{
-		actor = GetComponent<Rigidbody> ();
+		actor = GetComponent<Rigidbody>();
 	}
 
-	public void ReceiveDamage(float damage, Vector3 position) {
-
-		if (damage <= parameters.toughness) {
-			
+	public void ReceiveDamage(float damage, Vector3 position)
+	{
+		if (damage <= parameters.toughness)
+		{
 			parameters.toughness -= damage;
-			actor.AddExplosionForce (damage * parameters.damageFactor, position, 0.25f);
-
-			if (VFX.damageFX != null) {
-				GameObject newDamageFX = Instantiate (VFX.damageFX, position, Quaternion.identity);
-				Destroy (newDamageFX, 3);
+			actor.AddExplosionForce(damage * parameters.damageFactor, position, 0.25f);
+			if (VFX.damageFX != null)
+			{
+				GameObject newDamageFX = Instantiate(VFX.damageFX, position, Quaternion.identity);
+				Destroy(newDamageFX, 3);
 			}
-
-		} else {
-
-			if (VFX.deactivateFX != null) {
-				GameObject newDeactivateFX = Instantiate (VFX.deactivateFX, transform.position, Quaternion.identity);
-				Destroy (newDeactivateFX, 3);
+		}
+		else
+		{
+			if (VFX.deactivateFX != null)
+			{
+				GameObject newDeactivateFX = Instantiate(VFX.deactivateFX, transform.position, Quaternion.identity);
+				Destroy(newDeactivateFX, 3);
 			}
-
-			Destroy ();
+			Destroy();
 		}
 	}
 
-	private void OnCollisionEnter(Collision collision) {
-
-		if (collision.relativeVelocity.magnitude > parameters.armor) {
-			ReceiveDamage (collision.relativeVelocity.magnitude, transform.position);
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.relativeVelocity.magnitude > parameters.armor)
+		{
+			ReceiveDamage(collision.relativeVelocity.magnitude, transform.position);
 		}
 	}
 
-	public void Destroy() {
-
-		GetComponent<Collider> ().enabled = false;
-		GetComponent<Renderer> ().enabled = false;
-		GetComponent<AudioSource> ().PlayOneShot (SFX.destroyClip);
-		Destroy (gameObject, 2);
+	public void Destroy()
+	{
+		GetComponent<Collider>().enabled = false;
+		GetComponent<Renderer>().enabled = false;
+		GetComponent<AudioSource>().PlayOneShot(SFX.destroyClip);
+		Destroy(gameObject, 2);
 	}
 }
